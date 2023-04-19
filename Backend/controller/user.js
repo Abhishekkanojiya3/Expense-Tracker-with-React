881e181c59f5544a0276aae87606ac441f3b5d82
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-04-20 00:23:44 +0530

    Password encrption

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const bcrypt = require("bcrypt")
 
 function isStringInvalid(string) {
     if (string === undefined || string === null || string.length === 0) {
@@ -18,13 +19,16 @@ exports.postSignUpUser = async(req, res, next) => {
             console.log('Bad parameters');
             return res.status(400).json({ err: "Bad parameters..Something is missing" })
         }
-
-        await User.create({
-            email: email,
-            password: password
+        const saltrounds = 10;
+        bcrypt.hash(password, saltrounds, async(err, hash) => {
+            console.log(err)
+            await User.create({
+                email: email,
+                password: hash
+            })
+            console.log('User created');
+            res.status(201).json({ message: "You are successfully signed up" })
         })
-        console.log('User created');
-        res.status(201).json({ message: "You are successfully signed up" })
 
     } catch (err) {
         console.log('Error:', err);
@@ -32,27 +36,34 @@ exports.postSignUpUser = async(req, res, next) => {
     }
 }
 
-exports.postLoginUser = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log('email:', email);
-    console.log('password:', password);
-    User.findAll({ where: { email: email } })
-        .then((users) => {
-            const user = users[0]
-            console.log('user:', user);
-            if (!user) {
-                console.log('user not found');
-
-                return res.status(404).json({ err: "user not found" })
+exports.postLoginUser = async(req, res, next) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        console.log('email:', email);
+        console.log('password:', password);
+        if (isStringInvalid(email) || isStringInvalid(password)) {
+            return res.status(400).json({ err: "Email id or password is missing", success: false })
+        }
+        const users = await User.findAll({ where: { email: email } })
+        const user = users[0]
+        console.log('user:', user);
+        if (!user) {
+            console.log('user not found');
+            return res.status(404).json({ err: "user not found", success: false })
+        }
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+                throw new Error("Something went wrong")
             }
-
-            if (user.password != password) {
-                console.log('incorrect password');
-
-                return res.status(401).json({ err: "incorrect password" })
+            if (result === true) {
+                console.log('User logged in successfully');
+                res.status(200).json({ message: "User logged in successfully", success: true })
+            } else {
+                res.status(401).json({ err: "Incorrect Password", success: false })
             }
-            console.log('User logged in successfully');
-            res.status(200).json({ message: "User logged in successfully" })
         })
+    } catch (err) {
+        res.status(500).json({ message: err, success: false })
+    }
 }
